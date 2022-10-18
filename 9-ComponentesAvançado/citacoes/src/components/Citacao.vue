67c606d8e9c86ccc8144b993e80ed52b1e3b121e
dyomenegatti <dyo.menegatti@hotmail.com>
2022-10-18 14:44:48 -0300

    Finaliza seção 9: Uso Avançado de Componentes

---
+++ 9-ComponentesAvançado/citacoes/src/components/Citacao.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="citacao">
+      <!-- {{ texto }} -->
+      <div>
+        <slot name="autor"></slot>
+        <slot></slot>
+        <slot name="fonte"></slot>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ['texto']
+}
+</script>
+
+<style scoped>
+  .citacao {
+    border: 1px solid #DDD;
+    background-color: rgba(0, 0, 0, .1);
+    padding: 25px;
+    max-width: 450px;
+  }
+</style>
